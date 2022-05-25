7625cad49a306152b45e3dab42537854a7e469d4
GTM-Winter <dimastriana03@gmail.com>
2022-05-25 20:06:40 +0700

    menambah query login

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
 <?php
   include("koneksi.php");
+  $username=$_POST["use"];
+  $password=$_POST["pw"];
+  $tombol=$_POST["tombol"];
   
+  if ($tombol) 
+  {
+    $Query=mysqli_query($koneksi,"SELECT * FROM user WHERE md5(username)=md5('$username') AND password=md5('$password')");
+
+      if(mysqli_num_rows($Query))
+      {
+          $data=mysqli_fetch_assoc($Query);
+
+          $_SESSION['id']=$data['id_pengguna'];
+          $_SESSION['jenis_log']=$data['level'];
+          header("location:index.php");
+      }
+      else
+      {
+        $ket="Login gagal";
+      }
+  }
   echo "
   <!doctype html>
 <html lang='en'>
@@ -16,18 +36,18 @@
   </head>
   <body>
     <h1>Hello, world!</h1>
+    $ket
     <div class='container-fluid'>
-      <form name='POST' autocomplete='off'>
+      <form method='POST' autocomplete='off'>
         <div class='form-group'>
           <label for='username'>Username</label>
-          <input name='use' type='text' class='form-control' id='username' aria-describedby='emailHelp' placeholder='username'>
           <input name='use' type='text' class='form-control' id='username' aria-describedby='emailHelp' placeholder='username'>
         </div>
         <div class='form-group'>
           <label for='password'>Password</label>
           <input name='pw' type='password' class='form-control' id='password' placeholder='Password'>
         </div>
-        <input type='submit' class='btn btn-primary' value='Login'>
+        <input type='submit' class='btn btn-primary' value='Login' name='tombol'>
       </form>
     </div>
     <!-- Optional JavaScript -->
